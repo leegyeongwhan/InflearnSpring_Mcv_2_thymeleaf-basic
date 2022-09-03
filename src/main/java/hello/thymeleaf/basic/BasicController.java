630d5b915ebbfede9cc5564970ae29ac768c81e7
leegyeongwhan <leekhy02@naver.com>
2022-09-03 23:18:20 +0900

    feat: 타임리프 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -2,11 +2,13 @@ package hello.thymeleaf.basic;
 
 import lombok.Data;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,13 +46,28 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        private String hello(String data) {
+            return "hello" + data;
+        }
+    }
+
     @Data
     static class User {
         private String username;
         private int age;
+
         public User(String username, int age) {
             this.username = username;
             this.age = age;
         }
     }
+
 }
