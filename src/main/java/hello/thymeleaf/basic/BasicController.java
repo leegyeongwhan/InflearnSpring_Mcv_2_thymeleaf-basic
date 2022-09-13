7f3e089cec8368b2fb7beb4d71ad95fd8d8c365b
leegyeongwhan <leekhy02@naver.com>
2022-09-13 18:32:27 +0900

    feat: 자바스크립트 인라인

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -134,4 +134,12 @@ public class BasicController {
         addUsers(model);
         return "basic/block";
     }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 }
